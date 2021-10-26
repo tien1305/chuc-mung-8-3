8e09af902a7b8859639b667f6cc03c81113e256d
tien1305 <85263068+tien1305@users.noreply.github.com>
2021-10-26 19:08:55 +0700

    ..

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const nameGirl = 'Em';
 
 const giftUrl = 'http://nodemy.vn';
 
-const eventName = 'Chúc Mừng sinh nhật bạn;
+const eventName = 'Chúc Mừng sinh nhật em;
 const titleCard = 'Tặng người ấy
 
-const contentCard = 'Chúc em yêu của anh có ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const contentCard = 'Chúc em có ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
 
 const giftImage = 'hot-girl.png';
