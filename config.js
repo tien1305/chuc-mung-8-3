045398c417c40f05e0c1c79871ff4450840b4cfa
tien1305 <85263068+tien1305@users.noreply.github.com>
2021-10-26 19:42:24 +0700

    sâuwr

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const titleCard = 'Tặng người ấy'
 const contentCard = 'Chúc em có ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
 
-const giftImage = 'hot-girl.png';
+const giftImage = 'hot-girl.png.png';
 const base64 = 'anh yêu em';
 const giftImageBase64 = "data:image/png;base64, " + base64;
