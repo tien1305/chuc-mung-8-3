b70922212c82af5fba6bdb9d99c68ed37ad1cbeb
tien1305 <85263068+tien1305@users.noreply.github.com>
2021-11-18 20:51:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+const nameGirl = 'Ê';
 const giftUrl = 'http://nodemy.vn';
 
 const eventName = 'Chúc Mừng sinh nhật bạn';
@@ -6,6 +7,6 @@ const titleCard = 'Tặng người ấy'
 const contentCard = 'Chúc bạn có ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau';
 
 const giftImage = 'hot-girl.png';
-const base64 = 'sinh nhật vui vẽ.';
+const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
 
