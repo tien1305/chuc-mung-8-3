4b30fd4d723b0451b867abc5ef9fb85ddbbd4192
tien1305 <85263068+tien1305@users.noreply.github.com>
2022-03-08 16:39:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Gửi em';
 const giftUrl = 'http://nodemy.vn';
 
 const eventName = 'Chúc Mừng 8-3 ';
-const titleCard = 'Tặng Cô Quý '
+const titleCard = 'Tặng Em '
 
 const contentCard = 'Chúc em có ngày 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
